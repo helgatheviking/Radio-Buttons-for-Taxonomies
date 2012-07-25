fea6d11d69bcace6f99273fd247c91ca3b381bfd
kathy <helgatheviking@gmail.com>
2012-07-25 16:31:13 -0500

    fixed issue of jquery variable scope

--- inc/js/radiotax.js
+++ inc/js/radiotax.js
@@ -1,30 +1,79 @@
-jQuery(document).ready(function($) {  
+(function($) {
 
-	$('.radio-buttons-for-taxonomies').each(function(){
+/*
+ * SINGLE POST SCREEN
+ */
+ 
+	$('.radio-buttons-for-taxonomies').each(function(){  
 
-        id = $(this).attr('id');
+        var id = $(this).attr('id');
 
-        taxonomy = id.replace(/taxonomy-/g,""); 
+        var taxonomy = id.replace(/taxonomy-/g,"");  
 
         $('#' + taxonomy + 'checklist li :radio, #' + taxonomy + 'checklist-pop :radio').on('click', function(){  
-            var t = $(this), c = t.is(':checked'), id = t.val();  console.log(t); console.log(c); console.log(id);
+            var t = $(this), c = t.is(':checked'), id = t.val();  
             $('#' + taxonomy + 'checklist li :radio, #' + taxonomy + 'checklist-pop :radio').prop('checked',false);  
             $('#in-' + taxonomy + '-' + id + ', #in-popular-' + taxonomy + '-' + id).prop( 'checked', c );  
-        });  
 
-        $('#' + taxonomy + '-add').find('input.radio-tax-add').click(function(){ 
-		term = $('#' + taxonomy+'-add #new'+taxonomy).val(); 
-		nonce =$('#' + taxonomy+'-add #_wpnonce_radio-add-tag').val(); 
-		$.post(ajaxurl, { 
-			action: 'radio_tax_add_taxterm',
-			term: term,
-			'_wpnonce_radio-add-tag':nonce,
-			taxonomy: taxonomy
-			}, function(r){  
-				if(r!=-1) { 
-					$('#' + taxonomy + 'checklist').prepend(r.html).find('li#'+taxonomy+'-'+r.term+' :radio').attr('checked', true);
-				}
-			},'json');
-	    });  
+	    });  //end on radio click
+
+        $('#' + taxonomy + '-add').find('input.radio-tax-add').click(function(){  
+        		term = $('#' + taxonomy+'-add #new'+taxonomy).val(); 
+				nonce =$('#' + taxonomy+'-add #_wpnonce_radio-add-tag').val(); 
+
+				var request = $.ajax({
+				  type: "POST",
+				  url: ajaxurl,
+				  data: { action: "radio_tax_add_taxterm", '_wpnonce_radio-add-tag': nonce, 'taxonomy' : taxonomy, 'term' : term },
+				  beforeSend: function() {
+				     $('#' + taxonomy + '-ajax-spinner').show(); 
+				  }
+				});
+
+				request.fail(function(msg, textStatus) {   
+				  $('#' + taxonomy + '-ajax-response').addClass('error-message').text(msg); 
+				});
+
+				request.done(function(msg, textStatus) { 
+
+					var response = JSON.parse(msg);
+
+			  		//something went wrong in the admin side
+				  	if( typeof response.error != 'undefined') {
+				  	 	$('#' + taxonomy + '-ajax-response').addClass('error-message').text(response.error); 
+				  	 } 
+				  	 // term already exists
+				  	 else if (typeof response.hasterm != 'undefined' ) { 
+				  	 	//uncheck any 
+				  	 	$('#' + taxonomy + 'checklist li :radio, #' + taxonomy + 'checklist-pop :radio').prop('checked',false);  
+				  	 	//check the existing term
+				  	 	$('#' + taxonomy + 'checklist li :radio[value=' + response.hasterm + '], #' + taxonomy + 'checklist-pop :radio[value=' + response.hasterm + ']').prop('checked',true); 
+				  	 } 
+				  	 // if neither then we must be good to go
+				  	 else {
+				  	 	$('#' + taxonomy + 'checklist').prepend(response.html).find('li#'+taxonomy+'-'+response.term+' :radio').attr('checked', true);
+				  	 }
+
+				});
+
+				request.always(function() { 
+				  $('#' + taxonomy + '-ajax-spinner').hide();
+				});
+
+
+        }); //end on button click
+
     });//end each
-});  //end ready
+
+/*
+ * EDIT SCREEN
+ */
+
+
+$('.cat-checklist input').each(function(){
+	$(this).prop('type','radio');
+
+});
+
+
+})(jQuery);
