f5fbd6ecb3abb3d9cb77119fb24b9610426bd8a1
kathy <helgatheviking@gmail.com>
2012-09-07 12:58:31 -0400

    tweak Walker to support both hierarchical and non-hierarchical taxonomies

--- inc/class.Walker_Category_Radio.php
+++ inc/class.Walker_Category_Radio.php
@@ -5,7 +5,7 @@
 
 /**
  * Walker to output an unordered list of category radio <input> elements.
- * Duplicates Walker_Category_Checklist excerpt for the radio input
+ * Mimics Walker_Category_Checklist excerpt for the radio input
  *
  * @see Walker
  * @see wp_category_checklist()
@@ -26,7 +26,7 @@ class Walker_Category_Radio extends Walker {
         $output .= "$indent</ul>\n";
     }
 
-    function start_el( &$output, $category, $depth, $args, $id = 0 ) {
+    function start_el( &$output, $term, $depth, $args, $id = 0 ) {
         extract($args);
         if ( empty($taxonomy) )
             $taxonomy = 'category';
@@ -37,13 +37,13 @@ class Walker_Category_Radio extends Walker {
             $name = 'tax_input['.$taxonomy.']';
 
         //small tweak so that it works for both hierarchical and non-hierarchical tax
-        $value = is_taxonomy_hierarchical($taxonomy) ? $category->term_id : $category->slug;
+        $value = is_taxonomy_hierarchical($taxonomy) ? $term->term_id : $term->slug;
 
-        $class = in_array( $category->term_id, $popular_cats ) ? ' class="popular-category"' : '';
-        $output .= "\n<li id='{$taxonomy}-{$category->term_id}'$class>" . '<label class="selectit"><input value="' . $value . '" type="radio" name="'.$name.'[]" id="in-'.$taxonomy.'-' . $category->term_id . '"' . checked( in_array( $category->term_id, $selected_cats ), true, false ) . disabled( empty( $args['disabled'] ), false, false ) . ' /> ' . esc_html( apply_filters('the_category', $category->name )) . '</label>';
+        $class = in_array( $term->term_id, $popular_cats ) ? ' class="popular-category"' : '';
+        $output .= "\n<li id='{$taxonomy}-{$value}' $class>" . '<label class="selectit"><input value="' . $value . '" type="radio" name="'.$name.'[]" id="in-'.$taxonomy.'-' . $value . '"' . checked( in_array( $term->term_id, $selected_cats ), true, false ) . disabled( empty( $args['disabled'] ), false, false ) . ' /> ' . esc_html( apply_filters('the_category', $term->name )) . '</label>';
     }
 
-    function end_el( &$output, $category, $depth = 0, $args = array() ) {
+    function end_el( &$output, $term, $depth = 0, $args = array() ) {
         $output .= "</li>\n";
     }
 }
