c75dee6503ee0520274cfb079bd8af22e30b068d
kathy <helgatheviking@gmail.com>
2012-09-07 16:41:42 -0400

    prettify code

--- Radio-Buttons-for-Taxonomies.php
+++ Radio-Buttons-for-Taxonomies.php
@@ -39,7 +39,7 @@ To use this plugin, just activate it and go to the settings page.  Then Check th
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 
-if (!class_exists("Radio_Buttons_for_Taxonomies")) :
+if ( !class_exists( "Radio_Buttons_for_Taxonomies" ) ) :
 
 class Radio_Buttons_for_Taxonomies {
 
@@ -51,30 +51,30 @@ class Radio_Buttons_for_Taxonomies {
     	$this->plugin_url();
 
 	    // Include required files
-	    include_once('inc/class.WordPress_Radio_Taxonomy.php');
-      include_once('inc/class.Walker_Category_Radio.php');
+	    include_once( 'inc/class.WordPress_Radio_Taxonomy.php' );
+      include_once( 'inc/class.Walker_Category_Radio.php' );
 
 	    //create a class property for each taxonomy that we are converting to radio buttons
   		//for example: $this->categories
-  		$options = get_option('radio_button_for_taxonomies_options', true);  
-  		if(isset($options['taxonomies'])) foreach($options['taxonomies'] as $taxonomy) {  
-		     $this->{$taxonomy} = new WordPress_Radio_Taxonomy($taxonomy);
+  		$options = get_option( 'radio_button_for_taxonomies_options', true );  
+  		if( isset( $options['taxonomies'])) foreach( $options['taxonomies'] as $taxonomy ) {  
+		     $this->{$taxonomy} = new WordPress_Radio_Taxonomy( $taxonomy );
 		}
 
 	    // Set-up Action and Filter Hooks
-	    register_uninstall_hook(__FILE__, array(&$this,'delete_plugin_options'));
+	    register_uninstall_hook( __FILE__, array( &$this,'delete_plugin_options' ) );
 
 	    //load plugin text domain for translations
-	    add_action('plugins_loaded', array(&$this,'load_text_domain'));
+	    add_action( 'plugins_loaded', array( &$this,'load_text_domain' ) );
 
 	    //register settings
-	    add_action('admin_init', array(&$this,'admin_init' ));
+	    add_action( 'admin_init', array( &$this,'admin_init' ));
 
 	    //add plugin options page
-	    add_action('admin_menu', array(&$this,'add_options_page'));
+	    add_action( 'admin_menu', array( &$this,'add_options_page' ) );
 
 	    //add settings link to plugins page
-	    add_filter( 'plugin_action_links', array(&$this,'add_action_links'), 10, 2 );
+	    add_filter( 'plugin_action_links', array( &$this,'add_action_links' ), 10, 2 );
 
   }
 
@@ -85,7 +85,7 @@ class Radio_Buttons_for_Taxonomies {
 
 	function plugin_url() { 
 		if ( $this->plugin_url ) return $this->plugin_url;
-		return $this->plugin_url = plugins_url( basename( plugin_dir_path(__FILE__) ), basename( __FILE__ ) );
+		return $this->plugin_url = plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) );
 	}
 
 
@@ -95,8 +95,8 @@ class Radio_Buttons_for_Taxonomies {
 
   // Delete options table entries ONLY when plugin deactivated AND deleted
   function delete_plugin_options() {
-    $options = get_option('radio_button_for_taxonomies_options', true);
-    if(isset($options['delete'])) delete_option('radio_button_for_taxonomies_options');
+    $options = get_option( 'radio_button_for_taxonomies_options', true );
+    if( isset( $options['delete'] ) ) delete_option( 'radio_button_for_taxonomies_options' );
   }
 
   // ------------------------------------------------------------------------------
@@ -113,7 +113,7 @@ class Radio_Buttons_for_Taxonomies {
 
   // Init plugin options to white list our options
   function admin_init(){
-    register_setting( 'radio_button_for_taxonomies_options', 'radio_button_for_taxonomies_options', array(&$this,'validate_options') );
+    register_setting( 'radio_button_for_taxonomies_options', 'radio_button_for_taxonomies_options', array( &$this,'validate_options' ) );
   }
 
 
@@ -123,7 +123,7 @@ class Radio_Buttons_for_Taxonomies {
 
   // Add menu page
   function add_options_page() {
-    add_options_page(__('Radio Buttons for Taxonomies Options Page',"radio-buttons-for-taxonomies"), __('Radio Buttons for Taxonomies',"radio-buttons-for-taxonomies"), 'manage_options', 'radio-buttons-for-taxonomies', array(&$this,'render_form'));
+    add_options_page(__( 'Radio Buttons for Taxonomies Options Page',"radio-buttons-for-taxonomies" ), __( 'Radio Buttons for Taxonomies', "radio-buttons-for-taxonomies" ), 'manage_options', 'radio-buttons-for-taxonomies', array( &$this,'render_form' ) );
   }
 
 
@@ -133,7 +133,7 @@ class Radio_Buttons_for_Taxonomies {
 
   // Render the Plugin options form
   function render_form() {
-    include('inc/plugin-options.php');
+    include( 'inc/plugin-options.php' );
   }
 
   // Sanitize and validate input. Accepts an array, return a sanitized array.
@@ -142,12 +142,14 @@ class Radio_Buttons_for_Taxonomies {
     $clean = array();  
 
     //probably overkill, but make sure that the taxonomy actually exists and is one we're cool with modifying
-    $args=array('public'   => true,
-                'show_ui' => true);  
+    $args=array(
+      'public'   => true,
+      'show_ui' => true
+    );  
     
-    $taxonomies = get_taxonomies($args); 
-    if(isset($input['taxonomies'])) foreach ($input['taxonomies'] as $tax){
-    	if(in_array($tax,$taxonomies)) $clean['taxonomies'][] = $tax;
+    $taxonomies = get_taxonomies( $args ); 
+    if( isset( $input['taxonomies'] ) ) foreach ( $input['taxonomies'] as $tax ){
+    	if( in_array( $tax,$taxonomies ) ) $clean['taxonomies'][] = $tax;
     }
 
     $clean['delete'] =  isset( $input['delete'] ) ? 'true' : 'false' ;  //checkbox
@@ -159,9 +161,9 @@ class Radio_Buttons_for_Taxonomies {
   function add_action_links( $links, $file ) {
 
     if ( $file == plugin_basename( __FILE__ ) ) {
-      $posk_links = '<a href="'.admin_url('options-general.php?page=radio-buttons-for-taxonomies').'">'.__('Settings').'</a>';
+      $plugin_link = '<a href="'.admin_url( 'options-general.php?page=radio-buttons-for-taxonomies' ) . '">' . __( 'Settings' ) . '</a>';
       // make the 'Settings' link appear first
-      array_unshift( $links, $posk_links );
+      array_unshift( $links, $plugin_link );
     }
 
     return $links;
