2befcc703da31bcfaf837586d44eb615936f92ff
Kathy Darling <helgatheviking@gmail.com>
2013-05-16 12:21:53 -0500

    fix fatal error on updating settings (get_current_screen() not defined)

--- inc/class.WordPress_Radio_Taxonomy.php
+++ inc/class.WordPress_Radio_Taxonomy.php
@@ -288,7 +288,7 @@ class WordPress_Radio_Taxonomy {
 	 */
 	function get_terms ( $terms, $taxonomies, $args ){
 
-		if ( is_admin() && ! is_wp_error( $screen = get_current_screen() ) && in_array( $screen->id, array( 'post', 'edit-post' ) ) ) {
+		if ( is_admin() && function_exists( 'get_current_screen') && ! is_wp_error( $screen = get_current_screen() ) && in_array( $screen->id, array( 'post', 'edit-post' ) ) ) {
 
 			if( in_array( $this->taxonomy, ( array ) $taxonomies ) && ! in_array( 'category', $taxonomies ) ) {
 				$uncategorized = (object) array( 'term_id' => '0', 'slug' => '0', 'name' => 'No term' );
