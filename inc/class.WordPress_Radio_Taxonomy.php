694c04cece2290d255aebf2d5ad49900b2afd184
Kathy Darling <helgatheviking@gmail.com>
2013-05-16 13:09:45 -0500

    fix fatal error on settings save, fix improper html in metabox

--- inc/class.WordPress_Radio_Taxonomy.php
+++ inc/class.WordPress_Radio_Taxonomy.php
@@ -166,14 +166,14 @@ class WordPress_Radio_Taxonomy {
 					</h4>
 					<p id="<?php echo $taxonomy; ?>-add" class="category-add wp-hidden-child">
 						<label class="screen-reader-text" for="new<?php echo $taxonomy; ?>"><?php echo $tax->labels->add_new_item; ?></label>
-						<input type="text" name="new<?php echo $taxonomy; ?>" id="new<?php echo $taxonomy; ?>" class="form-required form-input-tip" value="<?php echo esc_attr( $tax->labels->new_item_name ); ?>" tabindex="3" aria-required="true"/>
+						<input type="text" name="new<?php echo $taxonomy; ?>" id="new<?php echo $taxonomy; ?>" class="form-required form-input-tip" value="<?php echo esc_attr( $tax->labels->new_item_name ); ?>" aria-required="true"/>
 						<label class="screen-reader-text" for="new<?php echo $taxonomy; ?>_parent">
 							<?php echo $tax->labels->parent_item_colon; ?>
 						</label>
 						<?php if( is_taxonomy_hierarchical($taxonomy) ) {
 							wp_dropdown_categories( array( 'taxonomy' => $taxonomy, 'hide_empty' => 0, 'name' => 'new'.$taxonomy.'_parent', 'orderby' => 'name', 'hierarchical' => 1, 'show_option_none' => '&mdash; ' . $tax->labels->parent_item . ' &mdash;', 'tab_index' => 3 ) );
 						} ?>
-						<input type="button" id="<?php echo $taxonomy; ?>-add-submit" class="add:<?php echo $taxonomy ?>checklist:<?php echo $taxonomy ?>-add button <?php if ( is_taxonomy_hierarchical ( $taxonomy ) ) { echo 'category-add-submit'; } else { echo 'radio-add-submit';} ?>" value="<?php echo esc_attr( $tax->labels->add_new_item ); ?>" tabindex="3" />
+						<input type="button" id="<?php echo $taxonomy; ?>-add-submit" data-wp-lists="add:<?php echo $taxonomy ?>checklist:<?php echo $taxonomy ?>-add" class="button <?php if ( is_taxonomy_hierarchical ( $taxonomy ) ) { echo 'category-add-submit'; } else { echo 'radio-add-submit';} ?>" value="<?php echo esc_attr( $tax->labels->add_new_item ); ?>" tabindex="3" />
 						<?php wp_nonce_field( 'add-'.$taxonomy, '_ajax_nonce-add-'.$taxonomy ); ?>
 						<span id="<?php echo $taxonomy; ?>-ajax-response"></span>
 					</p>
