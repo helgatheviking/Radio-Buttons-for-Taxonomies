90989cb85cde3bbe253a8b804df19e435fef9791
toscho <info@toscho.de>
2014-05-06 04:45:05 +0200

    clean up unused or inaccessible code, make option label clickable, fixes #20

--- inc/plugin-options.php
+++ inc/plugin-options.php
@@ -1,90 +1,84 @@
 <div class="wrap">
-  <div id="tabs">
+	<!-- Display Header, and Description -->
+	<style scoped="scoped">
+		fieldset {
+			font-size: 1.5em;
+			margin: 1em 0 .5em;
+		}
+	</style>
 
-  <style>
-    #nav-tabs { overflow: hidden; margin: 0 0 -1px 0;}
-    #nav-tabs li { float: left; margin-bottom: 0;}
-    .ui-tabs-nav a { color: #aaa;}
-    #nav-tabs li.ui-state-active a { border-bottom: 2px solid white; color: #464646; }
-    h2.nav-tab-wrapper { margin-bottom: 1em;}
-  </style>
+	<h2><?php _e( 'Radio Buttons for Taxonomies', 'radio-buttons-for-taxonomies' ); ?></h2>
 
-  <!-- Display Header, and Description -->
+	<!-- Beginning of the Plugin Options Form -->
+	<form method="post" action="<?php echo admin_url( 'options.php' ); ?>">
+		<?php settings_fields( 'radio_button_for_taxonomies_options' ); ?>
+		<?php $options = get_option( 'radio_button_for_taxonomies_options' ); ?>
 
-  <h2><?php _e('Radio Buttons for Taxonomies', 'radio-buttons-for-taxonomies' );?></h2>
+		<fieldset>
+			<legend><?php esc_html_e( 'Taxonomies', 'radio-buttons-for-taxonomies' ); ?></legend>
 
-  <!-- Beginning of the Plugin Options Form -->
-  <form method="post" action="<?php echo admin_url( 'options.php' );?>">
-    <?php settings_fields('radio_button_for_taxonomies_options'); ?>
-    <?php $options = get_option('radio_button_for_taxonomies_options');?>
+			<?php
+			$taxonomies = rbt_get_all_taxonomies();
 
-    <div id="general">
-        <fieldset>
-              <table class="form-table">
-                    <tr>
-                      <th scope="row"><?php _e( 'Select Taxonomies',  'radio-buttons-for-taxonomies' );?></th>
-                      <td>
+			if ( ! empty ( $taxonomies ) ) {
 
-                        <?php
+				foreach ( $taxonomies as $i => $taxonomy ) {
+					$checked = checked(
+						isset( $options[ 'taxonomies' ] ) && is_array( $options[ 'taxonomies' ] ) && in_array( $i, $options[ 'taxonomies' ] ),
+						1,
+						false
+					);
+					$id = "rbt_$i";
+					?>
+					<p>
+						<label for="<?php echo $id; ?>">
+							<input type="checkbox"
+								   name="radio_button_for_taxonomies_options[taxonomies][]"
+								   value="<?php echo $i; ?>" <?php echo $checked; ?>
+								   id="<?php echo $id; ?>"/>
+							<?php echo $taxonomy->labels->name; ?>
+						</label>
+					</p>
+				<?php
+				}
+			}
+			?>
+		</fieldset>
+		<fieldset>
+			<legend><?php esc_html_e( 'Options', 'radio-buttons-for-taxonomies' ); ?></legend>
+			<p>
+				<label for="rbt_delete">
+					<input type="checkbox"
+						   name="radio_button_for_taxonomies_options[delete]"
+						   id="rbt_delete"
+						   value="1" <?php checked( ! empty ( $options[ 'delete' ] ), 1 ); ?> />
+					<?php esc_html_e( 'Completely remove options on plugin removal', 'radio-buttons-for-taxonomies' ); ?>
+				</label>
+			</p>
+		</fieldset>
 
-                        $args=array(
-                            'public'   => true,
-                            'show_ui' => true,
-                            '_builtin' => true
-                          );
-
-                        $defaults = get_taxonomies( $args, 'objects' );
-                        ksort( $defaults );
-
-
-                        $args=array(
-                            'public'   => true,
-                            'show_ui' => true,
-                            '_builtin' => false
-                          );
-
-                        $custom = get_taxonomies( $args, 'objects' );
-                        ksort( $custom );
-
-
-                        $taxonomies = array_merge( $defaults , $custom );
-
-                        if( ! is_wp_error( $taxonomies ) ) {
-
-                          foreach ($taxonomies as $i=>$taxonomy)  {
-							  $checked = checked(
-								  isset($options['taxonomies']) && is_array($options['taxonomies']) && in_array($i, $options['taxonomies']),
-								  1,
-								  false
-							  );
-							  $id = "rbt_$i";
-							  ?>
-
-							  <label for="<?php echo $id; ?>">
-                            	<input type="checkbox" name="radio_button_for_taxonomies_options[taxonomies][]" value="<?php echo $i;?>" <?php echo $checked; ?> id="<?php echo $id; ?>" />
-							  <?php echo $taxonomy->labels->name; ?>
-							  </label><br/>
-
-                          <?php
-                              }
-
-                        } ?>
-
-                      </td>
-                    </tr>
-                    <tr>
-                      <th scope="row"><?php _e('Completely remove options on plugin removal', 'radio-buttons-for-taxonomies' );?></th>
-                      <td>
-                        <input type="checkbox" name="radio_button_for_taxonomies_options[delete]" value="1" <?php checked( isset( $options['delete'] ) && $options['delete'], 1 );?> />
-                      </td>
-                    </tr>
-                  </table>
-          </fieldset>
-      </div>
-
-          <p class="submit">
-                <input type="submit" class="button-primary" value="<?php _e( 'Save Changes', 'radio-buttons-for-taxonomies' ) ?>" />
-          </p>
-    </form>
-  </div>
+		<p class="submit">
+			<input type="submit" class="button-primary"
+				   value="<?php _e( 'Save Changes', 'radio-buttons-for-taxonomies' ) ?>"/>
+		</p>
+	</form>
 </div>
+<?php
+function rbt_get_all_taxonomies() {
+
+	$args = array (
+		'public'   => true,
+		'show_ui'  => true,
+		'_builtin' => true
+	);
+
+	$defaults = get_taxonomies( $args, 'objects' );
+	ksort( $defaults );
+
+	$args['_builtin'] = false;
+
+	$custom = get_taxonomies( $args, 'objects' );
+	ksort( $custom );
+
+	return array_merge( $defaults, $custom );
+}
