9c9da304da89c0f61a61672315fd02985081b404
Kathy Darling <helgatheviking@gmail.com>
2018-01-26 20:36:54 -0600

    minor typo

--- inc/class.Walker_Category_Radio.php
+++ inc/class.Walker_Category_Radio.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Core walker class to output an unordered list of category radio input elements.
- * Mimics Walker_Category_Checklist excerpt for the radio input /wp-admin/includes/class-walker-category-checklist.php from WordPress 4.4.0
+ * Mimics Walker_Category_Checklist except for the radio input /wp-admin/includes/class-walker-category-checklist.php from WordPress 4.4.0
  *
  * @since 1.0.0
  *
